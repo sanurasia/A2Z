a42fccc114cd0179db224a9a50d159f9a238ba19
sanjay <sanjay.csjm@gmail.com>
2019-09-06 13:50:09 +0530

    adding files for identification module

---
+++ A2ZLearn/ViewControllers/IdentifyLettersViewController.swift
@@ -0,0 +1,30 @@
+//
+//  IdentifyLettersViewController.swift
+//  A2ZLearn
+//
+//  Created by Sanjay.Chaurasia on 06/09/19.
+//  Copyright © 2019 sanjay kumar chaurasia. All rights reserved.
+//
+
+import UIKit
+
+class IdentifyLettersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
